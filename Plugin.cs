e495fa630d4f6956827f87f37ff0dfac89421c3f
Tyfyter <tyfyter2616@gmail.com>
2022-09-25 13:19:02 -0600

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,73 @@
+﻿using NetScriptFramework.SkyrimSE;
+using NetScriptFramework.Tools;
+using NetScriptFramework;
+using System.Reflection;
+using System.Linq;
+
+namespace FixOfSteel {
+    public class Plugin : NetScriptFramework.Plugin {
+        public override string Key => "fix.of.steel";
+        public static string PluginName => "Fix Of Steel";
+        public override string Name => PluginName;
+        public override int Version => 1;
+
+        public override string Author => "tyfyter";
+        public override string Website => "https://tyfyter.github.io/";
+
+        public override int RequiredFrameworkVersion => 9;
+        public override int RequiredLibraryVersion => 13;
+        BGSKeyword hAKwd;
+        BGSKeyword HeavyArmorKeyword => hAKwd ?? (hAKwd = TESForm.LookupFormById(0x6BBD2u) as BGSKeyword);
+        bool firstFrame = true;
+        bool inMenu = true;
+        protected override bool Initialize(bool loadedAny) {
+            if (!loadedAny) {
+                Utils.Log("Did not load FixOfSteel");
+                return false;
+            }
+            Events.OnMainMenu.Register((e) => {
+                Utils.Log($"menu event from {inMenu} to {e.Entering}");
+                inMenu = e.Entering;
+            });
+            Events.OnFrame.Register((e) => {
+                NetScriptFramework.SkyrimSE.Main instance = NetScriptFramework.SkyrimSE.Main.Instance;
+				if (instance is null || instance.IsGamePaused || !HeavyArmorKeyword.IsValid) return;
+                if (HeavyArmorKeyword is null || !HeavyArmorKeyword.IsValid) return;
+                if (PlayerCharacter.Instance is PlayerCharacter player && player.IsValid && HeavyArmorKeyword is BGSKeyword heavyArmorKeyword) {
+                    var gauntlets = player.GetEquippedArmorInSlot(EquipSlots.Hands);
+                    float value = 0;
+                    if (!(gauntlets is null || !gauntlets.HasKeyword(heavyArmorKeyword))) {
+                        value = gauntlets.ArmorRating * (1 + 0.4f * player.GetActorValue(ActorValueIndices.HeavyArmor) / 100f);
+                        player.ForceActorValue(ActorValueIndices.Variable02, gauntlets.Name.Length);
+                    }
+                    player.ForceActorValue(ActorValueIndices.Variable01, value);
+                    //player.ForceActorValue(ActorValueIndices.Variable02, player.ActiveEffects.Where(item => item?.BaseEffect?.PrimaryActorValue == ActorValueIndices.UnarmedDamage).Count());
+                    bool hasEffect = false;
+                    foreach (var item in player.ActiveEffects) if(!(item?.IsInactive??true) && item?.BaseEffect?.PrimaryActorValue == ActorValueIndices.UnarmedDamage && (item.Item.Name.Equals("Fists of Steel"))) {
+                        hasEffect = true;
+                        if (firstFrame) {
+                            Utils.Log(item.Item.Name + "; " + item.BaseEffect.Name + ": " + item.Magnitude);
+                        }
+                        Memory.WriteFloat(item.Address + 0x78, value);
+                        if (firstFrame) {
+                            Utils.Log(item.Item.Name + "; " + item.BaseEffect.Name + ": " + item.Magnitude);
+						}
+                    }
+                    if (hasEffect && firstFrame) {
+                        Utils.Log($"first frame, in menu:{inMenu}");
+                        firstFrame = false;
+                        if (!(gauntlets is null)) {
+                            //Utils.Log($"Members: {string.Join<MemberInfo>("\n", gauntlets.GetType().GetMembers())}");
+                            //foreach (var item in gauntlets.TypeInfo.Info.Fields) Utils.Log(item.ShortName+": "+item.TypeName);
+                        }
+                        foreach (var item in player.ActiveEffects) {
+                            //Utils.Log(item?.BaseEffect?.PrimaryActorValue + "; " + item?.Magnitude);
+                        }
+                    }
+                }
+            });
+            Utils.Log($"Loaded FixOfSteel, OwO");
+            return true;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FixOfSteel")]
+[assembly: AssemblyDescription("Fixes the Fists of Steel perk")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("tyfyter")]
+[assembly: AssemblyProduct("FixOfSteel")]
+[assembly: AssemblyCopyright("Copyright © tyfyter 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d76ab2c4-39e1-4097-9c95-b41eef91be3c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
